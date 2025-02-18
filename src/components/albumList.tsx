7e708d4001070f77edb82010872a93814a3875d2
martynfung <mknfung@gmail.com>
2025-02-18 11:26:49 -0800

    Add Prettier formatting and format all files

--- src/components/albumList.tsx
+++ src/components/albumList.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { AlbumCard } from "@/components/albumCard"
-import { Dropdown, DropdownOption } from "@/components/dropdown";
+import { AlbumCard } from "@/components/albumCard";
+import { Dropdown } from "@/components/dropdown";
 import { InfiniteScroll, ScrollItem } from "@/components/infiniteScroll";
 import { SearchInput } from "@/components/searchInput";
 import { albumSortOptions, SortKey } from "@/constants/albumSort";
@@ -10,81 +10,117 @@ import { Album } from "@/types/album";
 import * as React from "react";
 
 interface Props {
-    albums: ScrollItem<Album>[];
+  albums: ScrollItem<Album>[];
 }
 
 export function AlbumList({ albums }: Props): React.JSX.Element {
-    const [sortedAlbums, setSortedAlbums] = React.useState<ScrollItem<Album>[]>([])
-    const [sortKey, setSortKey] = React.useState<string | null>(null);
-    const [searchValue, setSearchValue] = React.useState<string>("");
-    const [genre, setSelectedGenre] = React.useState<string | null>(null);
+  const [sortedAlbums, setSortedAlbums] = React.useState<ScrollItem<Album>[]>(
+    []
+  );
+  const [sortKey, setSortKey] = React.useState<string | null>(null);
+  const [searchValue, setSearchValue] = React.useState<string>("");
+  const [genre, setSelectedGenre] = React.useState<string | null>(null);
 
-    const sortItems = (items: ScrollItem<Album>[], key: string | null, searchVal: string | null) => {
-        if (key === SortKey.top && !searchVal) {
-            return items;
-        }
-
-        const lowercasedSearchText = searchValue.toLowerCase();
-        const lowercasedGenre = genre?.toLocaleLowerCase() || "";
-        const filteredItems = items.filter((item) => {
-            const matchingAlbumTitle = item.data.albumTitle.toLowerCase().includes(lowercasedSearchText);
-            const matchingArtistName = item.data.artistName.toLowerCase().includes(lowercasedSearchText);
-            if (lowercasedGenre && lowercasedGenre !== "all") {
-                return (item.data.category.toLowerCase().includes(lowercasedGenre) && (matchingAlbumTitle || matchingArtistName))
-            }
-            return matchingAlbumTitle || matchingArtistName
-        }
-        )
-        console.log({ lowercasedGenre })
-        return filteredItems.sort((a, b) => {
-            switch (key) {
-                case SortKey.albumTitle:
-                    return a.data.albumTitle.localeCompare(b.data.albumTitle);
-                case SortKey.artistName:
-                    return a.data.artistName.localeCompare(b.data.artistName);
-                case SortKey.releaseDate:
-                    // Sort newest to oldest
-                    return b.data.releaseDate.getTime() - a.data.releaseDate.getTime();
-                default:
-                    return 0 // Default case if no valid sortKey is provided
-            }
-        });
-    };
-
-    React.useEffect(() => {
-        const sortedItems = sortItems([...albums], sortKey, searchValue);
-        setSortedAlbums(sortedItems)
-    }, [sortKey, albums, searchValue, genre])
-
-    function handleSelectSortOption(value: string) {
-        setSortKey(value)
+  const sortItems = (
+    items: ScrollItem<Album>[],
+    key: string | null,
+    searchVal: string | null
+  ) => {
+    if (key === SortKey.top && !searchVal) {
+      return items;
     }
 
-    function handleSearchValueChange(value: string) {
-        setSearchValue(value)
-    }
-
-    const renderItem = (album: ScrollItem<Album>) => <AlbumCard key={album.id} album={album.data} rank={album.index} />;
-
-    function renderListControls(): React.JSX.Element {
+    const lowercasedSearchText = searchValue.toLowerCase();
+    const lowercasedGenre = genre?.toLocaleLowerCase() || "";
+    const filteredItems = items.filter((item) => {
+      const matchingAlbumTitle = item.data.albumTitle
+        .toLowerCase()
+        .includes(lowercasedSearchText);
+      const matchingArtistName = item.data.artistName
+        .toLowerCase()
+        .includes(lowercasedSearchText);
+      if (lowercasedGenre && lowercasedGenre !== "all") {
         return (
-            <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
-                <SearchInput searchValue={searchValue} setSearchValue={handleSearchValueChange} debounceTimeMs={0} />
-                <Dropdown options={albumSortOptions} selectedOptionId={sortKey} handleSelectedOption={handleSelectSortOption} placeholderText="Sort by" />
-                <Dropdown options={genreOptions} selectedOptionId={genre} handleSelectedOption={setSelectedGenre} placeholderText="Filter" firstItemText="Filter by:" />
-            </div>
-        )
-    }
+          item.data.category.toLowerCase().includes(lowercasedGenre) &&
+          (matchingAlbumTitle || matchingArtistName)
+        );
+      }
+      return matchingAlbumTitle || matchingArtistName;
+    });
+    console.log({ lowercasedGenre });
+    return filteredItems.sort((a, b) => {
+      switch (key) {
+        case SortKey.albumTitle:
+          return a.data.albumTitle.localeCompare(b.data.albumTitle);
+        case SortKey.artistName:
+          return a.data.artistName.localeCompare(b.data.artistName);
+        case SortKey.releaseDate:
+          // Sort newest to oldest
+          return b.data.releaseDate.getTime() - a.data.releaseDate.getTime();
+        default:
+          return 0; // Default case if no valid sortKey is provided
+      }
+    });
+  };
 
+  React.useEffect(() => {
+    const sortedItems = sortItems([...albums], sortKey, searchValue);
+    setSortedAlbums(sortedItems);
+  }, [sortKey, albums, searchValue, genre, sortItems]);
+
+  function handleSelectSortOption(value: string) {
+    setSortKey(value);
+  }
+
+  function handleSearchValueChange(value: string) {
+    setSearchValue(value);
+  }
+
+  const renderItem = (album: ScrollItem<Album>) => (
+    <AlbumCard key={album.id} album={album.data} rank={album.index} />
+  );
+
+  function renderListControls(): React.JSX.Element {
     return (
-        <div>
-            <div className="flex flex-col items-center justify-between pb-6 gap-2 sm:flex-row">
-                <h1 className="text-xl font-bold">Top Albums</h1>
-                {renderListControls()}
-            </div>
-            <div id="infinite-scroll-container" className="min-h-screen max-w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-                <InfiniteScroll items={sortedAlbums} renderItem={renderItem} itemsPerPage={10} />
-            </div>
-        </div>
+      <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
+        <SearchInput
+          searchValue={searchValue}
+          setSearchValue={handleSearchValueChange}
+          debounceTimeMs={0}
+        />
+        <Dropdown
+          options={albumSortOptions}
+          selectedOptionId={sortKey}
+          handleSelectedOption={handleSelectSortOption}
+          placeholderText="Sort by"
+        />
+        <Dropdown
+          options={genreOptions}
+          selectedOptionId={genre}
+          handleSelectedOption={setSelectedGenre}
+          placeholderText="Filter"
+          firstItemText="Filter by:"
+        />
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-between pb-6 gap-2 sm:flex-row">
+        <h1 className="text-xl font-bold">Top Albums</h1>
+        {renderListControls()}
+      </div>
+      <div
+        id="infinite-scroll-container"
+        className="min-h-screen max-w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"
+      >
+        <InfiniteScroll
+          items={sortedAlbums}
+          renderItem={renderItem}
+          itemsPerPage={10}
+        />
+      </div>
+    </div>
+  );
 }
