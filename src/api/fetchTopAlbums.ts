073b6465064f4a0c5d5059030446141821447fa4
martynfung <mknfung@gmail.com>
2025-02-18 12:17:04 -0800

    Refactor out fetch api logic from home page

---
+++ src/api/fetchTopAlbums.ts
@@ -0,0 +1,38 @@
+import { Entry, ITunesResponse } from "@/types/iTunesResponse";
+import { ScrollItem } from "@/components/infiniteScroll";
+import { Album } from "@/types/album";
+
+const iTunesTopAlbumsUrl =
+  "https://itunes.apple.com/us/rss/topalbums/limit=100/json";
+
+export async function fetchTopAlbums(): Promise<ScrollItem<Album>[]> {
+  try {
+    const response = await fetch(iTunesTopAlbumsUrl);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch albums: ${response.statusText}`);
+    }
+
+    const data: ITunesResponse = await response.json();
+    return data.feed.entry.map((entry: Entry, index) => {
+      const album: Album = {
+        albumTitle: entry["im:name"].label || "",
+        artistName: entry["im:artist"].label || "",
+        artistLink: entry["im:artist"]?.attributes?.href || "",
+        albumLink: entry.link.attributes.href || "",
+        images: entry["im:image"] || "",
+        imageLink: entry["im:image"].at(-1)?.label || "",
+        releaseDate: new Date(entry["im:releaseDate"].attributes.label),
+        category: entry.category.attributes.label,
+      };
+
+      return {
+        index: index + 1,
+        id: entry.id.attributes["im:id"],
+        data: album,
+      };
+    });
+  } catch (error) {
+    console.error("Error fetching albums:", error);
+    return [];
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,9 @@
-import { Entry, ITunesResponse } from "@/types/iTunesResponse";
+import { fetchTopAlbums } from "@/api/fetchTopAlbums";
 import { AlbumList } from "@/components/albumList";
-import { ScrollItem } from "@/components/infiniteScroll";
-import { Album } from "@/types/album";
-
-const iTunesTopAlbumsUrl =
-  "https://itunes.apple.com/us/rss/topalbums/limit=100/json";
 
 export default async function Home(): Promise<React.JSX.Element> {
-  const response = await fetch(iTunesTopAlbumsUrl);
-  const data: ITunesResponse = await response.json();
+  const albums = await fetchTopAlbums();
 
-  const albums: ScrollItem<Album>[] = data.feed.entry.map(
-    (entry: Entry, index) => {
-      const album: Album = {
-        albumTitle: entry["im:name"].label || "",
-        artistName: entry["im:artist"].label || "",
-        artistLink: entry["im:artist"]?.attributes?.href || "", // TODO: Handle missing artist link
-        albumLink: entry.link.attributes.href || "", // TODO: Handle missing Link
-        images: entry["im:image"] || "",
-        imageLink: entry["im:image"].at(-1)?.label || "", // TODO: Handle missing link
-        releaseDate: new Date(entry["im:releaseDate"].attributes.label),
-        category: entry.category.attributes.label,
-      };
-
-      const scrollItemAlbum: ScrollItem<Album> = {
-        index: index + 1,
-        id: entry.id.attributes["im:id"],
-        data: album,
-      };
-
-      return scrollItemAlbum;
-    }
-  );
-
-  const firstEntry = data.feed.entry[0];
-  console.log(firstEntry);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
