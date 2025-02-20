a5494cc540f42d8526972049af85dcd9e18f7681
martynfung <mknfung@gmail.com>
2025-02-19 17:06:56 -0800

    Remove top gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home(): Promise<JSX.Element> {
 
   return (
     <FavoritesProvider>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-white">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 bg-white">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <AlbumList albums={albums} />
         </main>
